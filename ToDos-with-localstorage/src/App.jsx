b95af319b9f6abf13c6c38f992cbcbbea9073d98
aasif-iqbal <aasif.iqbal3000@gmail.com>
2025-07-11 16:38:31 +0530

    fix todo list

--- ToDos-with-localstorage/src/App.jsx
+++ ToDos-with-localstorage/src/App.jsx
@@ -5,14 +5,53 @@ import TodoItem from "./components/TodoItem";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const addTodo = () => {};
+  const [todos, setTodos] = useState([]); 
+  
+  const addTodo = (todo) => {
+    setTodos((prev) => [
+      { id: Date.now(), ...todo }, // new todo at the top
+      ...prev                      // spread old todos
+    ]);
+  };
+  
 
-  const updateTodo = () => {};
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
 
-  const deleteTodo = () => {};
+    // setTodos((prev) => {
+    //   prev.map((prevTodo)=>{
+    //     if(prevTodo.id === id){
+    //       return todo
+    //     }else{
+    //       return prevTodo
+    //     }
+    //   })
+    // })
+  };
 
-  const toggleComplete = () => {};
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+     //console.log(id);
+    setTodos((prev) => 
+      prev.map((prevTodo) => 
+        prevTodo.id === id ? { ...prevTodo, 
+          completed: !prevTodo.completed } : prevTodo))
+  };
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"));
+        
+    if(todos && todos.length > 0){
+      setTodos(todos);
+    }
+  },[]);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos]);
 
   return (
     <TodoProvider
@@ -30,7 +69,9 @@ function App() {
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
             {todos.map((todo) => (
-              <div key={todo.id} className="w-full">
+              <div key={todo.id}
+              className='w-full'
+              >
                 <TodoItem todo={todo} />
               </div>
             ))}
--- ToDos-with-localstorage/src/contexts/TodoContext.js
+++ ToDos-with-localstorage/src/contexts/TodoContext.js
@@ -2,11 +2,11 @@ import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
   todos:[
-    {
-      id:1,
-      todo_title:'todo msg',
-      completed:false
-    }
+    // {
+    //   id:1,
+    //   todo_title:'todo msg',
+    //   completed:false
+    // }
   ],
   addTodo : (todo_title) => {},
   updateTodo:(id, todo_title) => {},
